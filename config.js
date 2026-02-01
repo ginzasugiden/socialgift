b3a6ee68eb63e337b83d31d9220f6c5d7a96c891
ginzasugiden <ginzasugiden@gmail.com>
2026-02-01 16:05:29 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+  GAS_AUTH_URL:  'https://script.google.com/macros/s/AKfycbxzIr1GBlK706VxP6BXVBQDSbVyQF69rnI2DJvw2yxBP5ByulNdjMPjnVq1nhFXpucAqg/exec',
+  ACCESS_TOKEN:  'sgift_tokyoflower_20260201'
+};
